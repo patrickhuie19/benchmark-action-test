2dab3fa661cdfe1136323fd0d71117e6f6325b0a
github-action-benchmark <github@users.noreply.github.com>
2024-01-03 17:03:35 +0000

    add Benchmark (go) benchmark result for 76f58e5493cf40c6f5312599544d5dec2fc587ed

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1704301415064,
+  "repoUrl": "https://github.com/patrickhuie19/benchmark-action-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "patrick.huie@smartcontract.com",
+            "name": "patrickhuie19",
+            "username": "patrickhuie19"
+          },
+          "committer": {
+            "email": "patrick.huie@smartcontract.com",
+            "name": "patrickhuie19",
+            "username": "patrickhuie19"
+          },
+          "distinct": true,
+          "id": "76f58e5493cf40c6f5312599544d5dec2fc587ed",
+          "message": "Empty commit to test merge",
+          "timestamp": "2024-01-03T11:58:44-05:00",
+          "tree_id": "c72bd3336f2ff7bc4fbac93821b6313b45c038ab",
+          "url": "https://github.com/patrickhuie19/benchmark-action-test/commit/76f58e5493cf40c6f5312599544d5dec2fc587ed"
+        },
+        "date": 1704301414368,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAdd",
+            "value": 0.3103,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
