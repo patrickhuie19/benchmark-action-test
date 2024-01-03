d73351ee8871e777c6889689a75b546c9a399e43
patrickhuie19 <patrick.huie@smartcontract.com>
2024-01-03 11:43:43 -0500

    Violating expected benchmark ranges

--- main_test.go
+++ main_test.go
@@ -1,10 +1,15 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 // BenchmarkAdd is a dummy benchmark test to monitor for regressions in overal benchmark CI behavior.
 func BenchmarkAdd(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Add(1, 2)
+		time.Sleep(1 * time.Nanosecond)
 	}
+
 }
